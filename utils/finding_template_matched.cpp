4115fa7a11006819dcf960c6ba6dc9846523b950
Seongyoung-Kang <ksy9164@naver.com>
2020-12-15 06:25:36 -0800

    ADD: Debug Utils

---
+++ utils/finding_template_matched.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
+
+using namespace std;
+using namespace boost::algorithm;
+
+#define LOG_NUM 4
+
+const char log_file_name[LOG_NUM][100] = { "/mnt/hdd0/data/hpc4/bgl2.log",
+    "/mnt/hdd0/data/hpc4/liberty2.log",
+    "/mnt/hdd0/data/hpc4/spirit2.log", 
+    "/mnt/hdd0/data/hpc4/Thunderbird.log"};
+
+#define TABLE_FILE "./data/bgl2.txt"
+#define TEMPLATE_NUM 34
+
+int main(void)
+{
+    
+    /* Template reading */
+    ifstream in;
+    in.open(TABLE_FILE);
+    int template_number = TEMPLATE_NUM;
+
+    string line;
+    for (int i = 0; i < template_number; ++i) {
+        line = "";
+        getline(in, line);
+    }
+
+    vector<string> template_data;
+    split(template_data, line, is_any_of(" ")); // get template words
+
+    ifstream df;
+    df.open(log_file_name[0]);
+    int line_cnt = 0;
+    while (!df.eof()) {
+        string line;
+        getline(df, line);
+        int cnt = 0;
+        for (int i = 2; i < template_data.size(); ++i) {
+            if (line.find(template_data[i]) != std::string::npos) {
+                cnt++;
+            }
+        }
+        if (cnt >= template_data.size() - 2) {
+            cout << line_cnt << " " << line << endl;
+        }
+        line_cnt++;
+    }
+    return 0;
+}
--- utils/get_template_hash_matched.cpp
+++ utils/get_template_hash_matched.cpp
@@ -8,8 +8,7 @@
 using namespace std;
 using namespace boost::algorithm;
 
-#define F_TABLE "./data/hash_table.txt"
-#define F_TEMPLATE "./data/bgl2.txt"
+#define LOG_NUM 4
 
 uint8_t rand_generator(uint8_t old_rand) {
     uint8_t a = 133;
@@ -39,6 +38,13 @@ int hash_func2(string str){
     return idx;
 }
 
+int hit_table[4][256];
+
+const char log_file_name[LOG_NUM][100] = { "/mnt/hdd0/data/hpc4/bgl2.log",
+    "/mnt/hdd0/data/hpc4/liberty2.log",
+    "/mnt/hdd0/data/hpc4/spirit2.log", 
+    "/mnt/hdd0/data/hpc4/Thunderbird.log"};
+
 int main(void)
 {
 
@@ -64,6 +70,7 @@ int main(void)
     in.open("./data/bgl2.txt");
 
     int cnt = 1;
+    int hit_table_cnt = 0;
     while (!in.eof()) {
         string line;
         getline(in, line);
@@ -97,11 +104,66 @@ int main(void)
 
                 if (check) {
                     cout << "answer_t[" << (int)hash << "] = 1;" <<  endl;
+                    hit_table[hit_table_cnt][hash] = 1;
                 }
             }
+            hit_table_cnt++;
         }
 
         cnt++;
+    }
+    cout << "Setting is done" << endl;
+
+    /* Real data in */
+    ifstream df;
+    /* df.open(log_file_name[0]); */
+    df.open("./data/no_21_exceptional.txt");
+
+    while (!df.eof()) {
+        string line;
+        getline(df, line);
+        vector<string> token;
+        split(token, line, is_any_of(" "));
+        int temp_arr[256] = {};
+        for (int i = 0; i < token.size(); i++) {
+            uint8_t hash_a = hash_func1(token[i]);
+            uint8_t hash_b = hash_func2(token[i]);
+            string cmp;
+            if (token[i].size() > 16) {
+                cmp = token[i].substr(0, 16);
+            } else {
+                cmp = token[i];
+            }
+
+            uint8_t hash = 0;
+            bool check = false;
+
+            if (table[hash_a] == cmp) {
+                check = true;
+                hash = hash_a;
+            } else if (table[hash_b] == cmp) {
+                check = true;
+                hash = hash_b;
+            }
+
+            if (check) {
+                temp_arr[hash] = 1;
+            }
+        }
+
+        for (int i = 1; i < 2; i++) {
+            int ans = 1;
+            for (int j = 0; j < 256; ++j) {
+                if (temp_arr[j] != hit_table[i][j]) {
+                    ans = 0;
+                    cout << j << " " << temp_arr[j] << " " << hit_table[i][j] << endl;
+                }
+            }
+            if (ans == 1) {
+                cout << i << " : " << line << endl;
+            }
+            cout << "Line end" << endl;
+        }
     }
 
     return 0;
